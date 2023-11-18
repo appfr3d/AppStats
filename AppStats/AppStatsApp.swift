8a4fc824eea2fc9919cd720255934acf3ec6c0a8
Alfred Lieth Årøe <alfred.aroe97@gmail.com>
2023-11-18 15:26:30 +0100

    Initial Commit

---
+++ AppStats/AppStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStatsApp.swift
+//  AppStats
+//
+//  Created by Alfred Lieth Årøe on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
